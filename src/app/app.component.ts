3ae4a43386ea95f59f96b9f40f6ab1b304451252
Daniel Remsburg <danielremsburg@gmail.com>
2024-11-20 13:49:25 -0600

    Added the button with a stub to prove it works in the console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CliComponent } from './components/cli/cli.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TransportPlayButtonComponent } from "./transport-play-button/transport-play-button.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CliComponent, HttpClientModule],
+  imports: [RouterOutlet, CliComponent, HttpClientModule, TransportPlayButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
