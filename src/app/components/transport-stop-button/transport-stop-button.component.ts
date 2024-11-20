a77a115e6c2e2e72f4a3d60a6098bd190fbfccc4
Daniel Remsburg <danielremsburg@gmail.com>
2024-11-20 14:22:26 -0600

    Stop button functionality

--- src/app/components/transport-stop-button/transport-stop-button.component.ts
+++ src/app/components/transport-stop-button/transport-stop-button.component.ts
@@ -1,12 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { OscService } from '../../../services/osc.service';
 
 @Component({
   selector: 'app-transport-stop-button',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './transport-stop-button.component.html',
   styleUrl: './transport-stop-button.component.scss'
 })
 export class TransportStopButtonComponent {
+
+  constructor(private oscService: OscService) { }
+
+  stop() {
+    this.oscService.oscCommand = '/transport_stop';
+    this.oscService.sendOscCommand();
+  }
+
+
 
 }
