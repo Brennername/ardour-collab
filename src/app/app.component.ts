7e780e2465b08d9579afc5d44115d55a1960669f
Daniel Remsburg <danielremsburg@gmail.com>
2024-11-20 14:15:51 -0600

    Generated the component in the wrong dir, moved to the components dir.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CliComponent } from './components/cli/cli.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TransportPlayButtonComponent } from "./transport-play-button/transport-play-button.component";
+import { TransportPlayButtonComponent } from "./components/transport-play-button/transport-play-button.component";
 import { OscService } from '../services/osc.service';
 
 
--- src/app/components/transport-play-button/transport-play-button.component.ts
+++ src/app/components/transport-play-button/transport-play-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { OscService } from '../../services/osc.service';
+import { OscService } from '../../../services/osc.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
