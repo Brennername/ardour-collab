1d2961a61f6e4d66c18980888e98da8679e368e1
Daniel Remsburg <danielremsburg@gmail.com>
2024-11-20 14:25:07 -0600

    Stop button works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { CliComponent } from './components/cli/cli.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TransportPlayButtonComponent } from "./components/transport-play-button/transport-play-button.component";
 import { OscService } from '../services/osc.service';
+import { TransportStopButtonComponent } from "./components/transport-stop-button/transport-stop-button.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CliComponent, HttpClientModule, TransportPlayButtonComponent],
+  imports: [RouterOutlet, CliComponent, HttpClientModule, TransportPlayButtonComponent, TransportStopButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [OscService, HttpClient]
